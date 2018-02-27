131cec44d267c0497d726fccc5158b45eb8f2e3e
Viraj <salvi.viraj007@gmail.com>
2018-02-27 17:07:22 -0500

    Adding my pollme

--- src/pollme/polls/api/serializers.py
+++ src/pollme/polls/api/serializers.py
@@ -16,10 +16,36 @@ class QuestionListSerializer(ModelSerializer):
     Reference this stack overflow for the choices:
         https://stackoverflow.com/questions/33945148/return-nested-serializer-in-serializer-method-field
     """
-    pass
+    choices = SerializerMethodField()
+
+    def get_choices(self, obj):
+        choices = obj.choice_set.all()
+        return ChoiceSerailizer(choices, many=True).data
+
+    class Meta:
+        model = Polls
+        fields = ('id', 'text', 'pub_data', 'choices')
+
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Question.objects.create(**validated_data)
+
+
+
 
 class ChoiceSerializer(ModelSerializer):
     """
     This serializes the Choice model
     """
-    pass
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Choice.objects.create(**validated_data)
+
+    class Meta:
+        model = Polls
+        fields = ["choice_text","votes"]
--- src/pollme/polls/views.py
+++ src/pollme/polls/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
-
+rom django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+rom snippets.serializers import SnippetSerializer
 # Create your views here.
+
+
+@csrf_exempt
+def polls_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        polls = polls.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = QuestionListSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def polls_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        polls = polls.objects.get(pk=pk)
+    except polls.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = QuestionListSerializer(polls)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = QuestionListSerializer(polls, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        polls.delete()
+        return HttpResponse(status=204)
